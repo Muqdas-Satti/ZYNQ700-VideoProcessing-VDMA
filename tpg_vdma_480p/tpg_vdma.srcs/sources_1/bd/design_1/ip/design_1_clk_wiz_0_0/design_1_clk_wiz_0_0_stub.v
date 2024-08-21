311f70357c31ee15852131fe2902cab60553a8af
Muqdas-Satti <muqassatti16@gmail.com>
2024-08-21 11:48:04 +0500

    Your commit message

---
+++ tpg_vdma_480p/tpg_vdma.srcs/sources_1/bd/design_1/ip/design_1_clk_wiz_0_0/design_1_clk_wiz_0_0_stub.v
@@ -0,0 +1,20 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.1 (lin64) Build 2552052 Fri May 24 14:47:09 MDT 2019
+// Date        : Mon Aug 12 11:19:07 2024
+// Host        : latitude7400 running 64-bit Ubuntu 22.04.4 LTS
+// Command     : write_verilog -force -mode synth_stub
+//               /home/muqdas_satti/Desktop/SOC/vivado_projec/tpg_vdma_480p/tpg_vdma.srcs/sources_1/bd/design_1/ip/design_1_clk_wiz_0_0/design_1_clk_wiz_0_0_stub.v
+// Design      : design_1_clk_wiz_0_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg484-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+module design_1_clk_wiz_0_0(clk_out1, clk_in1)
+/* synthesis syn_black_box black_box_pad_pin="clk_out1,clk_in1" */;
+  output clk_out1;
+  input clk_in1;
+endmodule
---
+++ tpg_vdma_480p/tpg_vdma.srcs/sources_1/bd/design_1/ip/design_1_v_tpg_0_0/hdl/verilog/design_1_v_tpg_0_0_tpgPatternMask.v
@@ -0,0 +1,79 @@
+`pragma protect begin_protected
+`pragma protect version = 1
+`pragma protect encrypt_agent = "XILINX"
+`pragma protect encrypt_agent_info = "Xilinx Encryption Tool 2014"
+`pragma protect key_keyowner = "Aldec", key_keyname= "ALDEC15_001", key_method = "rsa"
+`pragma protect encoding = (enctype = "BASE64", line_length = 76, bytes = 256)
+`pragma protect key_block
+bOywaQ4mrvBk0TGN3+9vW8Pujf8EiGQKNRjJFmGzDVUWNd2g/2q956L677LvzVCpLJDOz30xuIJL
+GHVuKn6z49gK14OTyiI20dz+URf1u3waq8Jg5oxhwIfbcyKiPh5bt+YyuEzQ1Crw2fmsHLtcBnT8
+vL249obQy2uRBgXDWOd2m4QqPJRwRVKUuUtuLgCwcXVRQ6YZV42l4vyozjbcaLfNtrXK2FejBSjf
+/DPUQrGm769nEuMAbb4wCzMUyOdJ1voanBZ5XWdiWiwzu7biUdmaIE4Q7AbO5QCHJpOB7GI5rtib
+K3Qs4Xm6PE0Bo87ExkN8VROQ4EgZv+KRvmEheA==
+`pragma protect key_keyowner = "Cadence Design Systems.", key_keyname= "cds_rsa_key", key_method = "rsa"
+`pragma protect encoding = (enctype = "BASE64", line_length = 76, bytes = 64)
+`pragma protect key_block
+LRViRR/tsEq5NMew4Di/p/Sg+tB6+is6iVuWZREFJwbqcSV4LZazgjPf3ij7GF4y3HMgWI1ArH7N
+4oUklSMWIQ==
+`pragma protect key_keyowner = "Mentor Graphics Corporation", key_keyname= "MGC-VERIF-SIM-RSA-2", key_method = "rsa"
+`pragma protect encoding = (enctype = "BASE64", line_length = 76, bytes = 256)
+`pragma protect key_block
+dDdwKM1W7mcvzMl3dqyaN/71FFwdFqJ36MjJaLng+vFPyI+bCOP8U1IzG1SEu4ZJ6U0s8K2iTGSw
+Uxh72rC96aDzMR58vIrxJmgmt3rqUCAdkVNXO2GYSs85yzA+kFJ+ZXc1NLP+nN9qX315WAM24O99
+mF3k5lZCI0rs9mGskf7GEHsDAy0IDJb/EDNKoc1AFfz4ZTlmwlOXbfL2EDiKCWFHPJ2G7sUZZkKa
+A3ZIMyaa3nVTwyR2/J9/wYROHMrUl9KmQHgC9t31dnvsBKcqeeEVsic54y4NbDlJX5ujuwo/11XH
+t9Oak3fcVIbs9OfcH6Fsuu2wlcarEoyQbGuzrQ==
+`pragma protect key_keyowner = "Synopsys", key_keyname= "SNPS-VCS-RSA-2", key_method = "rsa"
+`pragma protect encoding = (enctype = "BASE64", line_length = 76, bytes = 128)
+`pragma protect key_block
+U3ZfUgNSIrrA9brkG4IGwbOu19XxnwxWGQbD+fVflM5oNNcz/AyK9MKEBwY3SASDtPcaecVKZAOR
+XjWlN7putfOZjD4NZGEOyryQCygf4mOh1W6Bdb+evKFYD1xoiqhUAErKte8WbcODPltR1rbgsVQJ
+JKe+Tsz+6JW3sOBYSvg=
+`pragma protect key_keyowner = "Xilinx", key_keyname= "xilinxt_2019_02", key_method = "rsa"
+`pragma protect encoding = (enctype = "BASE64", line_length = 76, bytes = 256)
+`pragma protect key_block
+cQ0Jtjvg7SSBVRh3erfyJFFr2jD+hYkR5u2tMuiiBvndPUjrbd4ZTdwr4+rbziuh7ERE1T2sU+cr
+YDbHe9NRrUpV9HDrZduUhPdDg5qvRJW4us2iwfDsmF9FDkANgtaDlr1X81BUmtLwidO4FfCACXqT
+V+JJIFSPCbfIElwfcWkNbHgOyJHvB7iiQeSqnxK1EyItLIKoYYZ3u+tYeBzv/TYN/GKjQp/+XouG
+jp8aUDTP5a3rEpbzzdNQsbrZxkgenCguO2RPfeeZrDhDjRbj+IQ7Udkk3kO+bNK1J5gZat4ywIRc
+MbS8HGO09tHAVPQ9EnZkY3EuLMuxv/ffOHBiwg==
+`pragma protect data_method = "AES128-CBC"
+`pragma protect encoding = (enctype = "BASE64", line_length = 76, bytes = 2000)
+`pragma protect data_block
+t7BTWI/hCHv+Bj2sCnTob/3r6Ks0qfTmRTsjNzYLZbc+os/qNgs5m8Fag32hHsl3ymwrtu2XFKhL
+XQ6fzBjzXQcz8p5qGV4BYJne0uNxCboDH6AMV155R9BRMpL584nz+OjJkpNiusq30/3QfSYaWI0+
+9V7liUD3Kc7DGOXs7f+edocWoKJYigPnvrTPWR6/I6kDbsy/giCc9tK81/URRbhiNBAScfkl1o/v
+JLk8kX7dFtr/lW7wXE5QnTHHPf61eE1zpuBV034/DW4LunE+l/oSWBWXAd/ETdf415H+5wbXgpcQ
+sqEbqNjCgYeO8+ORhl1FzGZAxNYx6Z+pRN/3AqEKXDFI1QyRf7ZJ1+zCN5NGQEAwyNE2LcEX5VZi
+zg5gLawU2F3IBFKAcPHuHUBz+tRMH0son/QYBykSZPZdhYfHePFO+veUdw8hNk7fPAkCx0Z8qmoO
+li6MZCDXkG3Kx47J6sglLIpllTJMTVm6044FNi+ET2ePvuno8LkW4y4tzN5FA1tOCtAZRl426LUn
+t2QzmpaSvSWIMgocg32DGgrobhsSRGx+KiLm+o+Glx0+jIQPZm8qLbi4zVVcXgp8hel+uXIHI+qV
+g+Firwb79P+krsiCIvQs9MADpG9juMVapGYy5N7XOWt/hVmXIMB1n2D+jrgBmihqrvQpFq1Svgh5
+6GkQAtVgtTV4pDn20qC6WE1ThFfh61aXCz8BOuc+YSbv65TPKgmNEfO0uNkkK1UDTKDDOf/t2tpD
+gZeNFd/UHn6jh9NPRfmqC4zEfdxb/5CA9Dqeg/yUSdpZl6vPj5YW1HKBN4kGug6RR30M1xcp6gBf
+QdHFpw1pSUVciUF8EfuVLvWbKRDl6ZObrcWEvNdEd0OMVMCl7jp4luJ1tNcSqwdO8f0rGfGXuL4Y
+GDlKvMPQ7wx9vUNln1wDqkTHh5NRGQMuJNibGhz8IlD/ej6CN9m/cRIPSa1uRW1BfS+6VfSt2pr9
+DusnqyFCOhtR/MJCRgdBWheq8rYXLYh/9IcSvrU5ED9g+NYYrqCpLy2qpRlVaZqR6mRBPllFfFSg
+Pd+d9+7oS1SjmuM7E5HhwD4W8kYtLPJzTfGnjKKje4C7J6MO6fupziQcZ2GopEH/ZSuJnfvjf0cg
+vR3tMxaXR+byEmlqHG7TEJ5tZYfM6CANjDjyR1fEteyLno/rEpqEmdCMqijKfU06HqS5Rf4lpdkX
+tUBtfXsBw2AM0FNBS1YyoQdj602K+Rf2Qdp0krRh3jlqCWDUcEb5Bf606oTSpJKtx4XaWssqB+nU
+VAk+BOAsOilUTaXxqJ9SDLY9ixVhhMOkXzkaCLd07sKrEmJPXY0e+ZLB4KKcvXx+SrQSK/ysr9oP
+KX4TjxdYaL8Auu/H9qBRWPZXuCIpIfuTDRBE5Vc9gxrTWyDRE04YDYfeIvoXIKjBK4hDfZLJUJF+
+2sdep7Yv5HtCuYgqFxdqxlFXaDrkQtf/cIdq8bAMTcN0OVM+Oq5iuw5/EzQehCghQVRmH9WdU3ar
+I1LWnMytgMYCy7B9JOZfGoEHWLNyMYvMy+l8tryIDFSmZ//RXxF+ncgUe1Xnnxyzi3hRf9BEdO8s
+llTFlDTRavkHh1h9jqNhGF3tEOCfeIDHkCq73g1iyqFMubHCDJVJ0FkMylTMmAJTFwyq1EcOYRKO
+iY4Ky2P3Z4Cu5rnv4Rmjq5JhZUNvMq5kwkHe7CfjKCtKih/iLyEf42Iv/0kfxkEZsI66rDiacpil
+xLaPDi9S7nclHjygbcmR/kNgUI4v0fnLJV7wtbdDZW+qDhkwJbh8IfYm8zN0566yWCcqOHqGXow4
+G6lK9gzwYZ71UBlpdqVdDt5JMqxtka/WAT5DToguWLv+UK3JSwmfyjA3UhQ6ivz7+2VBXj/cEgsT
+hGf11EYzM1oLNlkKuDbsty/s1LW416AihO1NRfbXvULd+6MOCf6JEbCG/8VhUx6q2S0nq+6bXPED
+A3B55L+c21eQgDD8VqEh0neNCCj0PfnKuKWoB8jXgDD21VHEWv9LdsswjvbjJrqIQuOKDadC5J17
+RVnbiXNQp4t6nT/SNwcLQb2octCQILxY+8m3geqRKE4VeOhY10k7mnD8IN/iELIk6rEVwvTyAZdN
+E+msqhZs9fufdE96iyGPW3ne9Jz9Gkxis1nikYW8cO5Q8WJS98eCTk/kkl6GqnWB0v/xOwVTYtKc
+mvO2DX40iIUeeRvfbeBZ+KNIM8q/bfOm13MDvRxc4FppY85iJaYW1i/eCxAZwKzjUW2+v6qZcMIU
+oa682TQKAhaROJZb+raRK0Ni/AB2u55aekT51w+rusd78HBuF6MOr1uVrcyVcp+nEwXtQMZeVMy8
+pv58It6ZaqILn224khjIza+PVfoJSlL9keKapmmwzl5y+Ksl/4vtkyokffjaadmCIsRBtJsz2jvK
+cdQyMhiNolBuUGsSwB8r3rKgiouuqmre1m8t2vT8/TkZuYkxXQImdAU3QA0eoh3i2QLoUKVbzauT
+Pm5U6uJgPdLzahB02pJVv1utmWwynX3WVm2jgpu9xJb4OFuNETnQuANZX5DsB+1IqaLBlONk/BX8
+IGRbjqVf8p9Xu4MspGkw8Z+ClFunlGQcPbBm8UchjLsVPvv+EEbeN8M7w8kjPOhQN2b+WFZpU97l
+mYrbrN0=
+`pragma protect end_protected
